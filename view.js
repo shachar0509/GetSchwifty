d3f3cf8fb1620b6b1db5f7c35eaeda0e4de690a2
shachar0509 <shachar0509@gmail.com>
2022-07-04 20:35:14 +0300

    added Page set function, sets board with onclick function injected

--- view.js
+++ view.js
@@ -1,3 +1,20 @@
+export function SetBoard(gameBoard, onClickFunc) {
+    var i = 0
+    gameBoard.forEach(box => {
+        if (i % Math.sqrt(gameBoard.length) === 0) {
+            document.body.appendChild(document.createElement("br"))
+        }
+        let element = document.createElement("button")
+        element.style.height = '100px'
+        element.style.width = '100px'
+        element.style.verticalAlign = 'top'
+        element.id = i
+        element.onclick = onClickFunc.bind(element)
+        element.innerHTML = box
+        document.body.appendChild(element);
+        i++;
+    });
+}
 export function FailNotify(){
     alert("Illegal Move")
 }
