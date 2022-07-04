55d14205850c5ad6b9aadcca81fc9652352711a8
shachar0509 <shachar0509@gmail.com>
2022-07-04 20:44:33 +0300

    added Init board function that contains nested GetRandomInt

---
+++ boardInitializer.js
@@ -0,0 +1,15 @@
+import Validate from "./boardValidator.js"
+export default function InitBoard(sizes) {
+    function GenerateBoard(size) {
+        var tempBoard = []
+        function enterElement(value) {
+            function getRandomInt() {
+                var rand = Math.floor(Math.random() * size)
+                return rand
+            };
+            
+        };
+    }
+    
+
+}
